18e0b0c84a507a8a55756f7c4580ddee1abaff7b
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2026-02-17 23:52:18 +0100

    feat: added two utilities to the library

---
+++ src/lib/chain.ts
@@ -0,0 +1,10 @@
+export const SEPOLIA = {
+  chainId: 11155111,
+  hexChainId: "0xaa36a7",
+  name: "Sepolia",
+  rpcHint: "https://rpc.sepolia.org",
+};
+
+export const CONTRACTS = {
+  kitties: "0x9F0a394A4e9722Ac21C0DA6887cAb3281a980075" as const,
+};
